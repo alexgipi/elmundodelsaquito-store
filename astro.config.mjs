25f1bc1360cc18e3ab6c686c13eabbbad8f7e501
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-02-28 13:40:55 +0100

    Use critters Astro integration for critical css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [svelte(), tailwind()],
+  integrations: [svelte(), tailwind(), critters()],
   adapter: vercel(),
   trailingSlash: 'never'
 });
