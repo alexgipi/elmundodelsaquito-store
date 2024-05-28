beca4b104cc456984cb12c62d9ed5519440d03a4
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-05-28 03:19:00 +0200

    Add new ORDER_STATUS_VALUES

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,34 @@
 export const ORDER_STATUS_VALUES:any = {
+    'pending': {
+      es: 'Pendiente',
+      en: 'Pending',
+    },
     'processing': {
       es: 'Procesando',
       en: 'Processing',
     },
+    'on-hold': {
+      es: 'En espera',
+      en: 'On Hold',
+    },
     'completed': {
       es: 'Completado',
       en: 'Completed',
-    }
+    },
+    'cancelled': {
+      es: 'Cancelado',
+      en: 'Cancelled',
+    },
+    'refunded': {
+      es: 'Reembolsado',
+      en: 'Refunded',
+    },
+    'failed': {
+      es: 'Fallido',
+      en: 'Failed',
+    },
+    'checkout-draft': {
+      es: 'Draft',
+      en: 'Draft',
+    },
 }
